7006f0a6309e1244dd469bfcf94ca9d155349d1b
beatrizHernandez <36974495+beatrizHernandez@users.noreply.github.com>
2020-09-22 14:06:51 -0500

    Add files via upload

--- Practica2/models/enlaces.php
+++ Practica2/models/enlaces.php
@@ -7,6 +7,19 @@
 			else if($enlaces == "index") {
 				$module = "views/registro.php";
 			}
+			else if($enlaces == "ok") {
+				$module = "views/modules/registro.php";
+			}
+			else if($enlaces == "fallo") {
+				$module = "views/modules/ingresar.php";
+			}
+			else if($enlaces == "cambio") {
+				$module = "views/modules/usuarios.php";
+			}
+			else {
+				$module = "views/modules/registro.php";
+			}
+			return $module;
 		}
 	}
 ?>
