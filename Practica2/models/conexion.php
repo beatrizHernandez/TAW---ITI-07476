91d55f308afd302b9c6b1c561a4fa26180c98d54
beatrizHernandez <36974495+beatrizHernandez@users.noreply.github.com>
2020-10-02 01:37:44 -0500

    Add files via upload

--- Practica2/models/conexion.php
+++ Practica2/models/conexion.php
@@ -1,7 +1,7 @@
 <?php
 	class Conexion {
 		public function conectar () {
-			$link = new PDO("mysql:host=localhost;dbname=practica2_bd","root","");
+			$link = new PDO("mysql:host=localhost;dbname=practica2_bd","root","HERNANDEZ99");
 			return $link;
 		}
 	}
