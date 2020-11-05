3f884af0479e7fabc8d1520c17ae5d0deacab500
beatrizHernandez <1730104@upv.edu.mx>
2020-11-05 16:38:52 -0600

    Actualización

--- Práctica6/receta-v2/database/seeds/UsuarioSeeder.php
+++ Práctica6/receta-v2/database/seeds/UsuarioSeeder.php
@@ -16,6 +16,15 @@ class UsuarioSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
+            'name'=>'Prueba usuario 1',
+            'email'=>'correo@correo.com',
+            'password'=>Hash::make('12345678'),
+            'url'=>'www.upv.com',
+            'created_at'=>date('Y-m-d H:i:s'),
+            'updated_at'=>date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
         	'name'=>'Beatriz Hdz',
         	'email'=>'correo1@correo.com',
         	'password'=>Hash::make('12345678'),
